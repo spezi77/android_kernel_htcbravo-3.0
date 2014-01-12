36ef375d48485fc7fc9162199c4f29fb881a9b35
Nikez <nikez141@gmail.com>
2014-01-12 22:09:33 +0100

    drivers: video: update to .38
    Conflicts:
    	arch/arm/mach-msm/include/mach/qdsp5v2/snddev_mi2s.h
    	drivers/video/msm/mdp_lcdc.c
    	drivers/video/msm/mdp_ppp.c

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1081,10 +1081,10 @@ static int msmfb_probe(struct platform_device *pdev)
 		/* Flip buffer */
 		msmfb->update_info.left = 0;
 		msmfb->update_info.top = 0;
-		msmfb->update_info.eright = msmfb->xres;
-		msmfb->update_info.ebottom = msmfb->yres;
-		msmfb_pan_update(msmfb->fb, 0, 0, msmfb->xres,
-				 msmfb->yres, 0, 1);
+		msmfb->update_info.eright = info->var.xres;
+		msmfb->update_info.ebottom = info->var.yres;
+		msmfb_pan_update(info, 0, 0, fb->var.xres,
+				 fb->var.yres, 0, 1);
 	}
 #endif
 	/* Jay, 29/12/08' */
